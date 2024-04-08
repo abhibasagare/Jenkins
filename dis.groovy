2fd07e2e3a0ba3ecb500127fcfdb37aa0f6dcf5c
abhi <abhibasagare@gmail.com>
2024-04-08 15:11:30 +0530

    First update

---
+++ dis.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Pull') {
+            steps {
+                echo 'This is pull operation'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'This is build operation'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                echo 'This is test operation'
+            }
+        }
+
+        stage('Quality check') {
+            steps {
+                echo 'This is quality check operation'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo 'This is deploy operation'
+            }
+        }
+    }
+}
